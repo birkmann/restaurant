c88ce47d84de47b0a8663e88ea3ac6cb387c57c5
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-12-04 14:17:30 +0100

    files

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		$("body").toggleClass('open');
 	});
 	$(".slider").slick({
-		autoplay: true,
+		autoplay: false,
   		autoplaySpeed: 5000,
 		prevArrow: '<button type="button" class="slick-prev svg slick-arrow"><svg viewBox="0 0 188.5 306"><polygon points="71 153 188.5 35.5 153 0 0 153 153 306 188.5 270.5 "></polygon></svg></button>',
 		nextArrow: '<button type="button" class="slick-next svg slick-arrow"><svg viewBox="0 0 188.5 306"><polygon points="117.5 153 0 270.5 35.5 306 188.5 153 35.5 0 0 35.5 "></polygon></svg></button>'
